cf5ef70105b6838a489daf70333c23c795ed0813
4010a7 <4010a7@corazon.ece.uprm.edu>
2016-08-31 08:40:23 -0400

    Changes
    The name of the frame was changed, the location more centralized and the
    size altered.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -4,7 +4,7 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Mariam");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(1000, 500);
+		myFrame.setLocation(1000, 450);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
